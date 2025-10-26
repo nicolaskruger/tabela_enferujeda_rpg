2aed3310595393432e7696e09742fbc9d4507832
ni <nicolas.nkruger@gmail.com>
2025-10-26 09:50:24 -0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ use tabela_enferujeda_rpg::rune;
 fn main() {
     let runes = rune::all_runes().join(", ");
     println!("{}", runes);
+    let rune_name = rune::all_runes().first().unwrap().to_string();
+    let decode = rune::rune_decoder(rune_name);
+    println!("{}", decode)
 }
--- src/rune/mod.rs
+++ src/rune/mod.rs
@@ -1,16 +1,26 @@
 use regex::Regex;
 use std::fs;
 
+const D_D_BOOK: &str = "./d&d_book/";
+
 fn pdf_regex() -> Regex {
     Regex::new(r"\.pdf$").unwrap()
 }
 
 pub fn all_runes() -> Vec<String> {
-    fs::read_dir("./d&d_book/")
+    fs::read_dir(D_D_BOOK)
         .into_iter()
         .flatten()
         .filter_map(|f| f.ok())
         .filter_map(|f| f.file_name().into_string().ok())
         .filter(|f| pdf_regex().is_match(f))
         .collect()
+}
+
+fn rune_full_path(rune_name: String) -> String {
+    format!("{}{}", String::from(D_D_BOOK), rune_name)
+}
+
+pub fn rune_decoder(rune_name: String) -> String {
+    pdf_extract::extract_text(rune_full_path(rune_name)).unwrap()
 }
