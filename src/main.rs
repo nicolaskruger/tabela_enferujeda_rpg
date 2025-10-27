c2f1880902358de0e32f41ffbf18255f7f5e1ba2
ni <nicolas.nkruger@gmail.com>
2025-10-27 07:43:05 -0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use tabela_enferujeda_rpg::rune;
 
+// fn lab00() {
+//     let runes = rune::all_runes().join(", ");
+//     println!("{}", runes);
+//     let rune_name = rune::all_runes().first().unwrap().to_string();
+//     let _decode = rune::rune_decoder(rune_name.clone());
+//     println!("decoded");
+//     let file_name = rune::swap_pdf_txt(rune_name.clone());
+//     rune::decod_save(file_name, _decode);
+// }
+
 fn main() {
-    let runes = rune::all_runes().join(", ");
-    println!("{}", runes);
-    let rune_name = rune::all_runes().first().unwrap().to_string();
-    let _decode = rune::rune_decoder(rune_name.clone());
-    println!("decoded");
-    let file_name = rune::swap_pdf_txt(rune_name.clone());
-    rune::decod_save(file_name, _decode);
+    let ctx = rune::decod_read("master.txt".to_string());
+
+    let res = rune::rune_content_do_dice(ctx);
+
+    res.iter().for_each(|dt| {
+        print!(
+            "title: {}, dice {}",
+            rune::dice_title(dt),
+            rune::dice_dice(dt)
+        )
+    })
 }
--- src/rune/mod.rs
+++ src/rune/mod.rs
@@ -3,6 +3,24 @@ use std::fs;
 
 const D_D_BOOK: &str = "./d&d_book/";
 
+pub struct Dice {
+    title: String,
+    lines: Vec<String>,
+    dice: String,
+}
+
+pub fn dice_title(dice: &Dice) -> String {
+    dice.title.to_string()
+}
+
+pub fn dice_dice(dice: &Dice) -> String {
+    dice.dice.to_string()
+}
+
+pub fn dice_lines(dice: &Dice) -> Vec<String> {
+    dice.lines.clone()
+}
+
 fn pdf_regex() -> Regex {
     Regex::new(r"\.pdf$").unwrap()
 }
@@ -31,4 +49,30 @@ pub fn swap_pdf_txt(file_name: String) -> String {
 
 pub fn decod_save(file_name: String, file_content: String) {
     fs::write(rune_full_path(file_name), file_content).unwrap();
+}
+
+pub fn decod_read(file_name: String) -> String {
+    fs::read_to_string(rune_full_path(file_name)).unwrap()
+}
+
+fn dice_regex() -> Regex {
+    Regex::new(r"^d\d+").unwrap()
+}
+
+pub fn rune_content_do_dice(content: String) -> Vec<Dice> {
+    let store: Vec<&str> = content.split("\n").collect();
+    let idxs: Vec<usize> = store
+        .iter()
+        .enumerate()
+        .filter(|x| dice_regex().is_match(x.1))
+        .map(|i| i.0)
+        .collect();
+
+    idxs.iter()
+        .map(|idx| Dice {
+            title: store[idx - 1].to_string(),
+            dice: store[*idx].to_string(),
+            lines: vec![],
+        })
+        .collect()
 }
